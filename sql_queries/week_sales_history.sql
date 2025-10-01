d6bfb33e6b3e29ba3ab402015ba8840037865700
Jenil Sam <jenilsam2004@gmail.com>
2025-10-01 09:34:27 -0500

    made changes to the view for week_sales_history

--- sql_queries/week_sales_history.sql
+++ sql_queries/week_sales_history.sql
@@ -1,8 +1,6 @@
-SELECT EXTRACT(YEAR FROM sales_time) AS year,
-       EXTRACT(WEEK FROM sales_time) AS week,
-       SUM(total_price) AS total_sales,
-       COUNT(*) AS num_orders
-FROM sales_history
-GROUP BY year, week
-ORDER BY year, week;
+SELECT 'WEEK ' || EXTRACT(WEEK FROM sales_time) || ' in ' || 'YEAR ' || EXTRACT(YEAR FROM sales_time) ||
+        ' has ' || COUNT(*) || ' orders' AS num_orders
+    FROM sales_history
+    GROUP BY EXTRACT(YEAR FROM sales_time), EXTRACT(WEEK FROM sales_time)
+    ORDER BY EXTRACT(YEAR FROM sales_time), EXTRACT(WEEK FROM sales_time);
 
