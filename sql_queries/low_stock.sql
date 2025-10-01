737a6f3682621058de12d538eab36a3a7f6fb5c3
Jenil Sam <jenilsam2004@gmail.com>
2025-10-01 18:08:22 -0500

    added comments

--- sql_queries/low_stock.sql
+++ sql_queries/low_stock.sql
@@ -1,3 +1,4 @@
+-- Shows the stock of ingredients below 500
 SELECT 
     'THESE ARE LOW: ' || ingredient_name || ' (Qty: ' || stock_quantity || ' ' || unit::text || ')' AS low_stock_items
 FROM ingredients
